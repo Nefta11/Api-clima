26190e329f5f568b5cd88a7812497a0f7854ab36
Nefta11 <neftaliarturohernandez@gmail.com>
2024-03-18 13:46:57 -0600

    Updated

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const result = document.querySelector('result')
-const form= document.querySelector('.get-weather')
-const nameCity = document.querySelector('#city')
-const nameCountry = document.querySelector('#country')
+const result = document.querySelector('.result'); // Corrige el selector para que apunte a la clase correcta
+const form = document.querySelector('.get-weather');
+const nameCity = document.querySelector('#city');
+const nameCountry = document.querySelector('#country');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if (nameCity.vale === '' || nameCountry.vale === ''){
-        showError('Ambos campos son de afuerza papi..!!');
+    if (nameCity.value === '' || nameCountry.value === ''){ 
+        showError('Ambos campos son obligatorios.');
     }
 
-    console.log(nameCity.vale)
-    console.log(nameCountry.vale)
-})
+    console.log(nameCity.value); 
+    console.log(nameCountry.value); 
+});
 
+function callAPI(city,country){
+    const apiId =  ' 82ab5004f81d26a35fa4e53e117e0038'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
+
+    fetch(url)
+        .then(data => {
+            return data.json();
+        })
+        .then(dataJSON =>{
+            console.log(dataJSON)
+        })
+}
 function showError(message){
     console.log(message);
     const alert = document.createElement('p');
