20069e9f92601a1c29aea56ee2ec45aa5d5e0bee
Nefta11 <neftaliarturohernandez@gmail.com>
2024-03-17 23:10:30 -0600

    Updated

--- index.js
+++ index.js
@@ -5,5 +5,23 @@ const nameCountry = document.querySelector('#country')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    if (nameCity.vale === '' || nameCountry.vale === ''){
+        showError('Ambos campos son de afuerza papi..!!');
+    }
+
     console.log(nameCity.vale)
+    console.log(nameCountry.vale)
 })
+
+function showError(message){
+    console.log(message);
+    const alert = document.createElement('p');
+    alert.classList.add('alert-message');
+    alert.innerHTML = message;
+
+    form.appendChild(alert);
+    setTimeout(()=>{
+        alert.remove();
+    }, 3000);
+}
