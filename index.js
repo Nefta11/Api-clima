1f03f3a6b0cf53e8461aa615c60750137cc64a99
Nefta11 <neftaliarturohernandez@gmail.com>
2024-03-17 22:44:37 -0600

    Updated Siuu

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const result = document.querySelector('result')
 const form= document.querySelector('.get-weather')
-const result = document.querySelector('result')
+const nameCity = document.querySelector('#city')
+const nameCountry = document.querySelector('#country')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(nameCity.vale)
+})
